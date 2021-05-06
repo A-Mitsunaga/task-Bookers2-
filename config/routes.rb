19edf09bb9ca0a1132cd6af49d1a9e6d3c190b43
A-Mitsunaga <a56st70410@icloud.com>
2021-05-06 10:45:43 +0000

    [Add]task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users
   root to: 'homes#top'
+post 'books/id' => 'books#create'
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 end
